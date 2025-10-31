2b072b031c17b57f76dab9ca6eea02d48024aaaf
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-31 22:25:45 +0530

    These are primitive data types

---
+++ basics/primitiveDatatypes.js
@@ -0,0 +1,51 @@
+// String
+let name = 'vinay' ;
+let greet = ` Hi ${name} , welcome `;
+
+console.log(greet);
+
+// Number
+let number1 = 34;
+let number2 = 1.2;
+
+console.log(number1 + number2)
+
+// boolean 
+
+let isStuding = false ;
+let isReading = true ;
+
+console.log( isStuding + " " + isReading) ;
+
+// undefined 
+
+let city ;
+
+console.log(city) ;
+
+// null 
+
+let car = null ;
+console.log(car) ;
+
+
+// symbol 
+
+let sym1 = Symbol("id") ;
+let sym2 = Symbol("id") ;
+
+console.log( sym1 === sym2) ;
+console.log( sym1 == sym2) ;
+
+// bigInt
+
+let bigNum = 12345678901234567890n;
+
+console.log(bigNum);''
+
+
+
+
+
+
+
