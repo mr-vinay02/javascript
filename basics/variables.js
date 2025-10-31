3fcc83d45d845ccaa61897fd3bf321e9b9fcd56e
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-31 21:32:16 +0530

    This are declaring the variables

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+var name = "vinay" ;
+console.log(name);
+
+
+var name = "Mari" ;  // redeclaring same variable
+console.log(name);
+
+let firstName = "vinay" ;
+console.log(firstName);
+
+const pi = 3.14;
+console.log(pi);
+
+// pi = 45;   // error : variable is a constant
+// console.log(pi);
+
+
