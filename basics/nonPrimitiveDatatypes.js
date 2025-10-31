68df5cc0c891370765ea3b83a03fb5afc699e108
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-31 22:50:31 +0530

    These are the Non Primitve Datatypes

---
+++ basics/nonPrimitiveDatatypes.js
@@ -0,0 +1,28 @@
+// Object 
+let person = {
+    name : "vinay" ,
+    age  : 22 ,
+    course : "java script"
+}  
+
+console.log(person);
+
+console.log(  ` ${person.name}    \n ${person.age}  \n  ${person.course} ` ) ;
+
+// Array
+
+let fruits = [ "Apple" , "Orange" , "banana"];
+
+console.log(fruits);
+
+// Function
+
+function greet()
+{
+    var name = "vinay" ;
+    console.log(`Hi , ${name}`)
+}
+
+greet() ;
+
+
