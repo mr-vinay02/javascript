b186f0049a2e8ac772aa4c36bac1a05eb7e0a7ed
mr-vinay02 <vinayvini4630@gmail.com>
2025-11-01 15:11:30 +0530

    This is a control statement

---
+++ basics/controlstatement.js
@@ -0,0 +1,34 @@
+// if  else if else statement
+
+let score = 85 ;
+
+if( score >= 85)
+    console.log("distinction");
+else if(score < 35)
+    console.log("fail");
+else
+    console.log("pass");
+
+
+// swithch statement
+
+let day = "3";
+
+switch(day){
+    case "1" : 
+        console.log("Monday");
+        break;
+    case "2" :
+        console.log("Tuesday");
+        break;
+    case "3" :
+    case "4" :
+        console.log("wednesday");
+        break;
+
+    default :
+    console.log("Invalid input")
+
+}
+
+
